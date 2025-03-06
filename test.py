397808f8991865ced828c3ff51c0d902b07ad2d4
Temjik <83635231+artbok@users.noreply.github.com>
2025-03-06 11:36:22 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-import requests
-url = 'http://127.0.0.1:5000/'
-page = 'editItem'
-data = {
-    "username": "eblanus",
-    "password": "hues0s",
-    "name": "bebra",
-    "description": None,
-    "quantity": 100 
-}
-request = requests.post(url+page, json=data)
-print(request.text)
 
-#  curl -X POST -H "Content-Type: application/json" -d '{"username": "bok", "password": "1"}' http://127.0.0.1:5000/authUser
